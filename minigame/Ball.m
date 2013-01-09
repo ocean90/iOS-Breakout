3c6c58f7f1e1d8287a3f13feaf0a90f53e1f1b3d
Dominik Schilling <dominikschilling+git@gmail.com>
2013-01-09 14:52:54 +0100

    Add collision detection

--- minigame/Ball.m
+++ minigame/Ball.m
@@ -19,25 +19,79 @@
 	if (self) {
 		self.layer.cornerRadius = 10;
 		_displayLink = [CADisplayLink displayLinkWithTarget:self selector:@selector(ballMoving:)];
-		
 	}
 	
 	return self;
 }
 
 -(void)ballMoving:(CADisplayLink *)displayLink {
-	CGFloat x = self.center.x + 1;
-	CGFloat y = self.center.y - 1;
+	CGFloat x = self.center.x - 2;
+	CGFloat y = self.center.y + 2;
 	
-	NSLog(@"Superview: %@, Self: %@", NSStringFromCGRect(self.superview.bounds), NSStringFromCGRect(self.frame));
-	
-	
-	if ( ! CGRectIntersectsRect( self.superview.bounds, self.frame ) ) {
+
+	CGRect rect1 = self.frame;
+	CGRect rect2 = self.superview.bounds;
+
+	if ( [self checkTopCollisionOf:rect1 in:rect2] ) {
+		NSLog(@"Top Collision");
+		[self stop];
+	};
+
+	if ( [self checkBottomCollisionOf:rect1 in:rect2] ) {
+		NSLog(@"Bottom Collision");
+		[self stop];
+	};
+
+	if ( [self checkLeftCollisionOf:rect1 in:rect2] ) {
+		NSLog(@"Left Collision");
+		[self stop];
+	};
+
+	if ( [self checkRightCollisionOf:rect1 in:rect2] ) {
+		NSLog(@"Right Collision");
+		[self stop];
+	};
+
+	/*if ( ! CGRectIntersectsRect( self.superview.bounds, self.frame ) ) {
 		NSLog(@"Intersection: %@", NSStringFromCGRect( CGRectIntersection( self.superview.bounds, self.frame ) ));
 		[self stop];
-	}
+	}*/
 
 	self.center = CGPointMake(x, y);
+}
+
+- (BOOL) checkTopCollisionOf:(CGRect)rect1 in:(CGRect)rect2 {
+	CGFloat point1 = CGRectGetMinY(rect1);
+	CGFloat point2 = CGRectGetMinY(rect2);
+	NSLog(@"%f - %f", point1, point2);
+
+	return point2 >= point1;
+}
+
+- (BOOL) checkBottomCollisionOf:(CGRect)rect1 in:(CGRect)rect2 {
+	CGFloat point1 = CGRectGetMaxY(rect1);
+	CGFloat point2 = CGRectGetMaxY(rect2);
+	NSLog(@"%f - %f", point1, point2);
+
+	return point2 <= point1;
+}
+
+- (BOOL) checkRightCollisionOf:(CGRect)rect1 in:(CGRect)rect2 {
+	CGFloat point1 = CGRectGetMaxX(rect1);
+	CGFloat point2 = CGRectGetMaxX(rect2);
+
+	NSLog(@"%f - %f", point1, point2);
+
+	return point2 <= point1;
+}
+
+- (BOOL) checkLeftCollisionOf:(CGRect)rect1 in:(CGRect)rect2 {
+	CGFloat point1 = CGRectGetMinX(rect1);
+	CGFloat point2 = CGRectGetMinX(rect2);
+
+	NSLog(@"%f - %f", point1, point2);
+
+	return point2 >= point1;
 }
 
 - (void) start {
